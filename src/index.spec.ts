671a2bbbdbb80befdf7807d9ed6fe6a53a95f64a
xiaolu-coding <454819375@qq.com>
2022-03-28 08:46:28 +0800

    chore: test.skip test.only

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,19 @@
-import { test, describe, expect } from 'vitest'
+import { test, describe, expect, assert } from 'vitest'
+// 
+describe('test', () => {
+  test("should work as expected", () => {
+    expect(Math.sqrt(4)).toBe(2)
+  })
+  // 跳过测试
+  test.skip("skipped test", () => {
+    // 跳过测试，没有错误
+    assert.equal(Math.sqrt(4), 3)
+  })
+  // 只运行这个测试
+  test.only("only test", () => {
+    assert.equal(Math.sqrt(4), 2)
+  })
+}) 
 
-test('should work as expected', () => {
-  expect(Math.sqrt(4)).toBe(2)
-})
+
+
