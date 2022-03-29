f00d38bfba58a2a913825ca419db577a039dc94c
xiaolu-coding <454819375@qq.com>
2022-03-29 09:31:28 +0800

    chore: toHaveProperty

--- src/expect.spec.ts
+++ src/expect.spec.ts
@@ -52,19 +52,19 @@ describe("expect", () => {
     expect(getApples2()).toBeUndefined()
   })
 
-  test('test expect toBeTruthy', () => {
+  test("test expect toBeTruthy", () => {
     // 检查是否等于true， 会将检查值转换为布尔值
     expect(true).toBeTruthy()
     expect(false).not.toBeTruthy()
   })
 
-  test('test expect toBeFalsy', () => {
+  test("test expect toBeFalsy", () => {
     // 检查是否等于false， 会将检查值转换为布尔值
     expect(false).toBeFalsy()
     expect(true).not.toBeFalsy()
   })
 
-  test('test expect toBeNull', () => {
+  test("test expect toBeNull", () => {
     function apples() {
       return null
     }
@@ -72,23 +72,23 @@ describe("expect", () => {
     expect(apples()).toBeNull()
   })
 
-  test('test expect toBeNaN', () => {
+  test("test expect toBeNaN", () => {
     // 检查是否等于NaN 是.toBe(NaN)的别名
     expect(NaN).toBeNaN()
     expect(1).not.toBeNaN()
   })
 
-  test('test expect toBeTypeOf', () => {
+  test("test expect toBeTypeOf", () => {
     // 检查是否等于指定的类型
-    expect('123').toBeTypeOf('string')
-    expect(123).toBeTypeOf('number')
-    expect(true).toBeTypeOf('boolean')
-    expect(undefined).toBeTypeOf('undefined')
-    expect({}).toBeTypeOf('object')
-    expect(() => {}).toBeTypeOf('function')
+    expect("123").toBeTypeOf("string")
+    expect(123).toBeTypeOf("number")
+    expect(true).toBeTypeOf("boolean")
+    expect(undefined).toBeTypeOf("undefined")
+    expect({}).toBeTypeOf("object")
+    expect(() => {}).toBeTypeOf("function")
   })
 
-  test('test expect toBeInstanceOf', () => {
+  test("test expect toBeInstanceOf", () => {
     class Stocks {
       val: number
       constructor(val: number) {
@@ -101,49 +101,49 @@ describe("expect", () => {
     expect(stock).toBeInstanceOf(Stocks)
   })
 
-  test('test expect toBeGreaterThan', () => {
+  test("test expect toBeGreaterThan", () => {
     // 用于检查值是否大于接收值，相等和小于都无法通过测试
     expect(3).toBeGreaterThan(2)
     expect(2).not.toBeGreaterThan(3)
     expect(2).not.toBeGreaterThan(2)
   })
 
-  test('test expect toBeGreaterThanOrEqual', () => {
+  test("test expect toBeGreaterThanOrEqual", () => {
     // 用于检查值是否大于等于接收值
     expect(3).toBeGreaterThanOrEqual(2)
     expect(3).toBeGreaterThanOrEqual(3)
     expect(3).not.toBeGreaterThanOrEqual(4)
   })
 
-  test('test expect toBeLessThan', () => {
+  test("test expect toBeLessThan", () => {
     // 用于检查值是否小于接收值，如果相等和大于都无法通过测试
     expect(2).toBeLessThan(3)
     expect(2).not.toBeLessThan(2)
     expect(3).not.toBeLessThan(2)
   })
 
-  test('test expect toBeLessThanOrEqual', () => {
+  test("test expect toBeLessThanOrEqual", () => {
     expect(2).toBeLessThanOrEqual(3)
     expect(2).toBeLessThanOrEqual(2)
     expect(3).not.toBeLessThanOrEqual(2)
   })
 
-  test('test expect toEqual', () => {
+  test("test expect toEqual", () => {
     const stockBill = {
-      type: 'apples',
-      count: 13
+      type: "apples",
+      count: 13,
     }
 
     const stockMary = {
-      type: 'apples',
-      count: 13
+      type: "apples",
+      count: 13,
     }
     // 检查值是否等于接收值，或者是同样的结构，如果是对象类型（将会使用递归的方法进行比较）
     expect(stockBill).toEqual(stockMary)
     expect(1).toEqual(1)
   })
 
-  test('test expect toStrictEqual', () => {
+  test("test expect toStrictEqual", () => {
     class Stock {
       type: string
       constructor(type) {
@@ -155,31 +155,57 @@ describe("expect", () => {
     expect(new Stock("apples")).not.toStrictEqual({ type: "apples" })
   })
 
-  test('test expect toContain', () => {
+  test("test expect toContain", () => {
     // 用于检查数组和字符串中是否包含接收值
-    expect('xiaolu').toContain('xiao')
+    expect("xiaolu").toContain("xiao")
     expect([1, 2, 3]).toContain(2)
   })
 
-  test('test expect toContainEqual', () => {
+  test("test expect toContainEqual", () => {
     // 用于检查数组中是否包含具有特定结构和值的元素
     // 它就像对每个元素进行 toEqual 操作。
-    const stock = [{
-      fruit: 'apples',
-      count: 5
-    }]
+    const stock = [
+      {
+        fruit: "apples",
+        count: 5,
+      },
+    ]
     expect([1, 2, 3]).toContainEqual(2)
-    expect(stock).not.toContainEqual({ fruit: 'apples' })
-    expect(stock).toContainEqual({ fruit: 'apples', count: 5 })
+    expect(stock).not.toContainEqual({ fruit: "apples" })
+    expect(stock).toContainEqual({ fruit: "apples", count: 5 })
   })
 
-  test('test expect toHaveLength', () => {
+  test("test expect toHaveLength", () => {
     //  用于断言一个对象是否具有 .length 属性，并且它被设置为某个数值。
     expect("abc").toHaveLength(3)
     expect([1, 2, 3]).toHaveLength(3)
     expect({ length: 3 }).toHaveLength(3)
     // length值不是3
     expect([1, 2, 3, 4]).not.toHaveLength(3)
-    
+  })
+
+  test("test expect toHaveProperty", () => {
+    const invoice = {
+      isActive: true,
+      customer: {
+        first_name: "John",
+        last_name: "Doe",
+        location: "China",
+      },
+      total_amount: 5000,
+      items: [
+        {
+          type: "apples",
+          quantity: 10,
+        },
+        {
+          type: "oranges",
+          quantity: 5,
+        },
+      ],
+    }
+    expect(invoice).toHaveProperty("isActive") // 断言密钥存在
+    expect(invoice).toHaveProperty("total_amount", 5000) //断言 key 存在且值相等
+    expect(invoice).not.toHaveProperty("account") //断言这个 key 不存在
   })
 })
