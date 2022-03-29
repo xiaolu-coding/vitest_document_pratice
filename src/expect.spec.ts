3a22a1bb62d79dd96d26ec2806445d961546e6be
xiaolu-coding <454819375@qq.com>
2022-03-29 08:10:55 +0800

    chore: expect.not

--- src/expect.spec.ts
+++ src/expect.spec.ts
@@ -6,4 +6,10 @@ describe('expect', () => {
     expect(input).toBe(2)  // Jest API
     expect(input).to.equal(2) // chai API
   })
+
+  test("test expect not", () => {
+    const input = Math.sqrt(16)
+    expect(input).not.toBe(4) // Jest API
+    expect(input).not.to.equal(4) // chai API
+  })
 })
