2bcf2c2108576366ecbceff64b9f7613f71cc546
xiaolu-coding <454819375@qq.com>
2022-03-30 08:13:52 +0800

    chore: afterEach

--- src/setupAndTeardown.spec.ts
+++ src/setupAndTeardown.spec.ts
@@ -1,10 +1,17 @@
-import { describe, expect, test, beforeEach } from "vitest"
+import { describe, expect, test, beforeEach, afterEach } from "vitest"
 
-describe('test beforeEach', () => {
+describe('test setup and teardown', () => {
   test('test beforeEach', () => {
     // 注册一个回调,在当前上下文中的每个测试运行之前被调用
     beforeEach(() => {
       console.log("beforeEach")
+    })
+  })
+
+  test('test afterEach', () => {
+    // 注册一个回调,在当前上下文中的每个测试运行之后被调用
+    afterEach(() => {
+      console.log("afterEach")
     })
   })
 })
